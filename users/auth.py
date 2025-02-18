4f7c648975b283d2e20a427b2898842ba63bf475
LaraGomezN <111064261+LaraGomezN@users.noreply.github.com>
2025-02-18 18:26:53 -0500

    users

---
+++ users/auth.py
@@ -0,0 +1,89 @@
+from datetime import timedelta, datetime, timezone
+from typing import Annotated
+from fastapi import APIRouter, Depends, HTTPException
+from pydantic import BaseModel
+from sqlalchemy.orm import Session
+from starlette import status
+from database import SessionLocal
+from models import Users
+from passlib.context import CryptContext
+from fastapi.security import OAuth2PasswordRequestForm, OAuth2PasswordBearer
+from jose import jwt, JWTError
+
+#import secrets
+#print(secrets.token_urlsafe(50))
+
+router = APIRouter(
+    prefix = "/auth",
+    tags = ["auth"]
+)
+
+SECRET_KEY = "OSyy8YWFQZE4OSFqDVNUu502pHA_x7k8Sd2FpYdiEaoka7i-VH3bh_W9HOUk-rD2Nbc"
+ALGORITHM = "HS256"
+
+bcrypt_context = CryptContext(schemes=["bcrypt"], deprecated="auto")
+oauth2_bearer = OAuth2PasswordBearer(tokenUrl="auth/token")
+
+class CreateUserRequest(BaseModel):
+    name: str
+    email: str
+    password: str
+
+class Token(BaseModel):
+    access_token: str
+    token_type: str
+
+def get_db():
+    db = SessionLocal()
+    try:
+        yield db
+    finally:
+        db.close()
+
+db_dependency = Annotated[Session, Depends(get_db)]
+
+@router.post("/", status_code=status.HTTP_201_CREATED) #Verificar la ruta /users
+async def createUser(db: db_dependency, create_user_request: CreateUserRequest):
+    create_user_model = Users(
+        name = create_user_request.name,
+        email = create_user_request.email,
+        password = bcrypt_context.hash(create_user_request.password)
+    )
+    db.add(create_user_model)
+    db.commit()
+
+@router.post("/token", response_model=Token)
+async def login_for_access_token(form_data: Annotated[OAuth2PasswordRequestForm, Depends()], db: db_dependency):
+    user = authenticate_user(form_data.username, form_data.password, db)
+    if not user:
+        raise HTTPException(status_code=status.HTTP_401_UNAUTHORIZED, detail="No se pudo validar el usuario.")
+    
+    token = create_access_token(user.name, user.id, timedelta(minutes=20))
+
+    return {"access_token": token, "token_type": "bearer"}
+
+
+def authenticate_user(username: str, password:str, db):
+    user = db.query(Users).filter(Users.name == username).first()
+    if not user:
+        return False
+    if not bcrypt_context.verify(password, user.password):
+        return False
+    return user
+    
+def create_access_token(username:str, user_id:str, expires_delta: timedelta):
+    encode = {"sub": username, "id": user_id}
+    expires = datetime.now(timezone.utc) + expires_delta
+    encode.update({"exp": expires})
+    return jwt.encode(encode, SECRET_KEY, algorithm=ALGORITHM)
+
+async def get_current_user(token: Annotated[str, Depends(oauth2_bearer)]):
+    try:
+        payload = jwt.decode(token, SECRET_KEY, algorithms=[ALGORITHM])
+        username: str = payload.get("sub")
+        user_id: int = payload.get("id")
+        if username is None or user_id is None:
+            raise HTTPException(status_code=status.HTTP_401_UNAUTHORIZED, detail="No se pudo validar el usuario.")
+        return {"username": username, "id": user_id}
+    except JWTError:
+        raise HTTPException(status_code=status.HTTP_401_UNAUTHORIZED, detail="No se pudo validar el usuario.")
---
+++ users/main.py
@@ -0,0 +1,55 @@
+from fastapi import FastAPI, HTTPException, Depends
+from pydantic import BaseModel
+from typing import Annotated
+import models
+from starlette import status
+from database import engine, SessionLocal
+from sqlalchemy.orm import Session
+import auth
+from auth import get_current_user
+
+app = FastAPI()
+app.include_router(auth.router)
+models.Base.metadata.create_all(bind=engine) #Create table and columns in postgres
+
+class UserBase(BaseModel):
+    name: str
+    email: str
+    password: str
+
+def get_db():
+    db = SessionLocal()
+    try:
+        yield db
+    finally:
+        db.close()
+
+
+db_dependency = Annotated[Session, Depends(get_db)]
+user_dependency = Annotated[dict, Depends(get_current_user)]
+
+#POST
+"""@app.post("/users")
+async def createUser(user: UserBase, db: db_dependency):
+    db_user = models.Users(name = user.name, email = user.email, password = user.password)
+    db.add(db_user)
+    db.commit()
+    db.refresh(db_user)"""
+
+#GET
+"""@app.get("/users/{user_id}")
+async def getUser(user_id: int, db: db_dependency):
+    result = db.query(models.Users).filter(models.Users.id == user_id).first()
+    if not result:
+        raise HTTPException(status_code=404, deatil='Usuario no encontrado')
+    return result"""
+
+
+#GET con validacion
+@app.get("/", status_code=status.HTTP_200_OK)
+async def user(user: user_dependency, db: db_dependency):
+    if user is None:
+        raise HTTPException(status_code=401, deatil='No se pudo validar el usuario.')
+    return {"User": user}
+
+
