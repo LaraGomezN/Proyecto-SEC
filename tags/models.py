ba7f447f35ca262e217aa86483baa10ce479eb35
Pablo <p.laraf@uniandes.edu.co>
2025-02-19 22:23:23 -0500

    tags

---
+++ tags/models.py
@@ -0,0 +1,8 @@
+from sqlalchemy import Column, Integer, String
+from database import Base
+
+class Tags(Base):
+    __tablename__ = 'tags'
+
+    id = Column(Integer, primary_key=True, index=True)
+    topic = Column(String, unique=True, index=True)
